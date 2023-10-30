4f582bfb0f6ea66405fdeaa8e4a667861c8bb44c
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-10-30 23:56:27 +0000

    add style to PageError

--- src/views/page404/PageError.jsx
+++ src/views/page404/PageError.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   Box,
   Card,
@@ -9,8 +10,15 @@ import {
   Stack,
 } from "@mui/material/";
 import mainBg from "@assets/images/bg.png";
+// import four from "../../assets/icons/four.svg";
+// import zero from "../../assets/icons/zero.svg";
 
 export default function PageError() {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate(`/home`);
+  };
   return (
     <Box
       sx={{
@@ -25,13 +33,39 @@ export default function PageError() {
         justifyContent: "center",
       }}
     >
-      <Box>
-        <Typography>404</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: (theme) => theme.palette.background.paper,
+          // maxWidth: "440px",
+          padding: "86px 182px",
+          borderRadius: "16px",
+        }}
+      >
+        <Typography
+          component="h1"
+          variant="h1"
+          sx={{
+            fontSize: "200px",
+            color: (theme) => theme.palette.primary.main,
+          }}
+        >
+          404
+        </Typography>
         <Typography>Oops! Сторінку не знайдено</Typography>
-        <Typography>
+        <Typography sx={{ pt: "20px" }}>
           Перевірте URL адресу сторінки або поверніться на головну{" "}
         </Typography>
-        <Button>Перейти на головну</Button>
+        <Button
+          type="button"
+          variant="contained"
+          onClick={handleClick}
+          sx={{ mt: "36px" }}
+        >
+          Перейти на головну
+        </Button>
       </Box>
     </Box>
   );
