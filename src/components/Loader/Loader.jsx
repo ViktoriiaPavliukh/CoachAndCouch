0e0e628fe52e6c980207965a607846912160ef50
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-30 23:28:03 +0000

    fix build

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,10 @@
-import { Box } from "@material-ui/core";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import { Box, CircularProgress } from "@mui/material";
 
 export default function Loader() {
   return (
-    <Box sx={{ display: "flex", gap: 2, alignItems: "center", flexWrap: "wrap" }}>
+    <Box
+      sx={{ display: "flex", gap: 2, alignItems: "center", flexWrap: "wrap" }}
+    >
       <CircularProgress />
     </Box>
   );
