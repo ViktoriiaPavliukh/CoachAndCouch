6ad1fff5133dad2324ba39bf9e4f9e8cf3bba34d
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-03-15 12:58:48 +0200

    apadtive

--- src/components/PersonalAccount/Messages/Messages.jsx
+++ src/components/PersonalAccount/Messages/Messages.jsx
@@ -23,6 +23,7 @@ export const Messages = () => {
   // const messages = useSelector(selectMessages);
   // const intl = useIntl();
   const [userChat, setUserChat] = useState("");
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
   const sortedChats = messages.sort((a, b) => {
     const lastMessageA = a.messages[a.messages.length - 1];
@@ -43,16 +44,111 @@ export const Messages = () => {
     setUserChat(sortedChats[0]);
     // dispatch(getUserMessages());
   }, []);
+  const handleSidebarChatClick = (chat) => {
+    setUserChat(chat);
+    setIsChatOpen(true);
+  };
 
   return (
-    <Box sx={{ ml: "20px" }}>
+    <Box sx={{ width: "100%" }}>
       {sortedChats.length !== 0 ? (
-        <Stack sx={{ display: "flex", flexDirection: "row" }}>
+        <Stack
+          sx={{
+            display: { xs: "flex", md: "none" },
+            flexDirection: { xs: "column" },
+          }}
+        >
+          {isChatOpen && userChat ? (
+            <ChatWithUser user={userChat} />
+          ) : (
+            <Box
+              sx={{
+                width: { xs: "100%", md: "260px", lg: "212px" },
+                height: "100vh",
+                p: {
+                  sm: "32px 16px",
+                  md: "32px 12px 32px 60px",
+                  lg: "40px 12px",
+                },
+                boxShadow: "0px 1px 4px 0px rgba(0, 0, 0, 0.10)",
+              }}
+            >
+              <List>
+                {messages.map((chat) => {
+                  console.log(chat);
+                  const sortedMessages = [...chat.messages].sort((a, b) => {
+                    return new Date(b.writtedAt) - new Date(a.writtedAt);
+                  });
+                  const lastMessage = sortedMessages[0];
+                  const isSelected = userChat === chat;
+                  return (
+                    <React.Fragment key={chat.userCorrespondenceId.id}>
+                      <ListItem
+                        onClick={() => {
+                          handleSidebarChatClick(chat);
+                        }}
+                        sx={{
+                          m: "12px 0",
+                          backgroundColor: isSelected
+                            ? "#D1D5DB"
+                            : "transparent",
+                          "&:hover": { background: "#F3F4F6" },
+                        }}
+                      >
+                        <ListItemAvatar>
+                          <Avatar>
+                            <Aperture />
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={chat.userCorrespondenceId.name}
+                          primaryTypographyProps={{
+                            fontWeight: !lastMessage.isReaded ? "700" : "400",
+                            overflow: "hidden",
+                            whiteSpace: "nowrap",
+                            textOverflow: "ellipsis",
+                          }}
+                          secondary={lastMessage.message}
+                          secondaryTypographyProps={{
+                            fontWeight: !lastMessage.isReaded ? "700" : "400",
+                            overflow: "hidden",
+                            whiteSpace: "nowrap",
+                            textOverflow: "ellipsis",
+                          }}
+                        />
+                      </ListItem>
+                      <Box
+                        sx={{
+                          borderBottom: "1px solid #9CA3AF",
+                        }}
+                      />
+                    </React.Fragment>
+                  );
+                })}
+              </List>
+            </Box>
+          )}
+        </Stack>
+      ) : (
+        <p> у вас ще немає повідомлень</p>
+      )}
+      {sortedChats.length !== 0 ? (
+        <Stack
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: { md: "row" },
+            justifyContent: { md: "center" },
+          }}
+        >
           <Box
             sx={{
-              width: "320px",
+              width: { xs: "100%", md: "260px", lg: "212px" },
               height: "100vh",
-              p: "40px 12px",
+              p: {
+                sm: "32px 16px",
+                md: "32px 12px 32px 60px",
+                lg: "40px 12px",
+              },
               boxShadow: "0px 1px 4px 0px rgba(0, 0, 0, 0.10)",
             }}
           >
@@ -86,10 +182,16 @@ export const Messages = () => {
                         primary={chat.userCorrespondenceId.name}
                         primaryTypographyProps={{
                           fontWeight: !lastMessage.isReaded ? "700" : "400",
+                          overflow: "hidden",
+                          whiteSpace: "nowrap",
+                          textOverflow: "ellipsis",
                         }}
                         secondary={lastMessage.message}
                         secondaryTypographyProps={{
                           fontWeight: !lastMessage.isReaded ? "700" : "400",
+                          overflow: "hidden",
+                          whiteSpace: "nowrap",
+                          textOverflow: "ellipsis",
                         }}
                       />
                     </ListItem>
