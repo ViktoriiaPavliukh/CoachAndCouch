99903eecee2e73d58ecf1347e3ace583b23a0c6d
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-06-20 00:16:19 +0300

    fix hoppage/countryCase

--- src/helpers/countriesCase.js
+++ src/helpers/countriesCase.js
@@ -1,6 +1,7 @@
 export default function countriesCase(countrie) {
+  if (!countrie) return "";
   return countrie
     .split(" ")
-    .map((el) => el[0].toUpperCase() + el.toLowerCase().substring(1, el.length))
+    .map((el) => el[0].toUpperCase() + el.toLowerCase().substring(1))
     .join(" ");
 }
