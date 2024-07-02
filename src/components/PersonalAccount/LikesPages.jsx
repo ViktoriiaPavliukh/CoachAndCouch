1508e90362844e21b103b7214b313dbf880d5f84
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-07-02 20:52:36 +0100

    add TeacherCards to page favorite

--- src/components/PersonalAccount/LikesPages.jsx
+++ src/components/PersonalAccount/LikesPages.jsx
@@ -1,75 +1,78 @@
+import { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Grid, Typography } from "@mui/material";
+import Loader from "../Loader/Loader";
+import { getCurrentUser } from "@/redux/users/operations";
+import { fetchLikedAdverts } from "@/redux/marketplace/adverts/operations";
+import {
+  likesSelector,
+  selectAdvertsIsLoading,
+  advertsSelector,
+} from "@/redux/marketplace/adverts/advertsSelector";
 import {
   selectCurrentUser,
   selectUserIsLoading,
 } from "@/redux/users/selectors";
-import { useEffect, useState } from "react";
-import { useIntl } from "react-intl";
-import { useDispatch, useSelector } from "react-redux";
-import Loader from "../Loader/Loader";
-import { getCurrentUser } from "@/redux/users/operations";
-import {
-  advertsSelector,
-  selectAdvertsIsLoading,
-} from "@/redux/marketplace/adverts/advertsSelector";
-import { getAdverts } from "@/redux/marketplace/adverts/operations";
-import { Box, Typography } from "@mui/material";
+import { TeacherCard } from "../Teachers/TeacherCard";
 
 export function LikesPages() {
   const dispatch = useDispatch();
   const intl = useIntl();
+  const adverts = useSelector(advertsSelector) || [];
+  console.log("adverts", adverts);
   const currentUser = useSelector(selectCurrentUser);
+  console.log(currentUser);
   const isLoadingUser = useSelector(selectUserIsLoading);
   const isLoadingAdverts = useSelector(selectAdvertsIsLoading);
-  const adverts = useSelector(advertsSelector); // Make sure this is an array
-  const [likedAdverts, setLikedAdverts] = useState([]);
+  const likedAdverts = useSelector(likesSelector);
+  console.log("114", likedAdverts);
+  const [likedAdvertIds, setLikedAdvertIds] = useState([]);
 
   useEffect(() => {
     dispatch(getCurrentUser());
-    dispatch(getAdverts());
   }, [dispatch]);
 
   useEffect(() => {
-    if (currentUser && currentUser.id && adverts) {
-      // Ensure adverts is an array
-      const advertsArray = adverts.adverts || []; // Adjust according to actual data structure
-
-      // Filter adverts based on the current user's likes
-      const likedAdverts = advertsArray.filter((advert) =>
-        advert.likes.some((like) => like.id === currentUser.id)
-      );
-      setLikedAdverts(likedAdverts);
+    console.log("Current User:", currentUser);
+    if (currentUser && currentUser.likes && currentUser.likes.length > 0) {
+      const ids = currentUser.likes.map((like) => like.advert.id);
+      console.log("Liked Advert IDs:", ids);
+      setLikedAdvertIds(ids);
+    } else {
+      // Handle case where likes are not available or empty
+      setLikedAdvertIds([]);
     }
-  }, [currentUser, adverts]);
+  }, [currentUser]);
+
+  useEffect(() => {
+    if (likedAdvertIds.length > 0) {
+      dispatch(fetchLikedAdverts(likedAdvertIds));
+    }
+  }, [dispatch, likedAdvertIds]);
+  console.log(133, likedAdvertIds);
 
   const isLoading = isLoadingUser || isLoadingAdverts;
 
-  return isLoading ? (
-    <Loader />
-  ) : likedAdverts.length > 0 ? (
-    <Box>
-      {likedAdverts.map((advert) => (
-        <Box key={advert.id}>
-          <Typography>{advert.description}</Typography>
-          <img
-            src={advert.imagePath}
-            alt={advert.description}
-            width="100"
-            height="80"
-          />
-        </Box>
-      ))}
-    </Box>
-  ) : (
-    <Typography
-      sx={{
-        display: "flex",
-        width: "100%",
-        height: "50vh",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {intl.formatMessage({ id: "noAdvertisement" })}
-    </Typography>
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          flexDirection: { sm: "column", md: "row" },
+          columnGap: { md: "28px" },
+          rowGap: { xs: "28px", lg: "40px" },
+          justifyContent: "left",
+          ml: { lg: "48px" },
+          py: "38px",
+        }}
+      >
+        {adverts.map((advert) => (
+          <Grid item key={advert.id}>
+            <TeacherCard teacher={advert} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
