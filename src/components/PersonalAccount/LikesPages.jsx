d7cda6378997efc57f1a3608bfd0e235c34bcc21
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-07-03 20:48:47 +0100

    gix list of favorite

--- src/components/PersonalAccount/LikesPages.jsx
+++ src/components/PersonalAccount/LikesPages.jsx
@@ -6,7 +6,6 @@ import Loader from "../Loader/Loader";
 import { getCurrentUser } from "@/redux/users/operations";
 import { fetchLikedAdverts } from "@/redux/marketplace/adverts/operations";
 import {
-  likesSelector,
   selectAdvertsIsLoading,
   advertsSelector,
 } from "@/redux/marketplace/adverts/advertsSelector";
@@ -19,56 +18,49 @@ import { TeacherCard } from "../Teachers/TeacherCard";
 export function LikesPages() {
   const dispatch = useDispatch();
   const intl = useIntl();
-  const adverts = useSelector(advertsSelector) || []; // Ensure adverts is an array
-  console.log("Adverts:", adverts);
-
+  const adverts = useSelector(advertsSelector) || [];
   const currentUser = useSelector(selectCurrentUser);
-  console.log("Current User:", currentUser);
-
   const isLoadingUser = useSelector(selectUserIsLoading);
   const isLoadingAdverts = useSelector(selectAdvertsIsLoading);
-  const likedAdverts = useSelector(likesSelector) || [];
-  console.log("Liked Adverts:", likedAdverts);
-
   const [likedAdvertIds, setLikedAdvertIds] = useState([]);
-  console.log(likedAdvertIds);
 
   useEffect(() => {
     dispatch(getCurrentUser());
   }, [dispatch]);
 
   useEffect(() => {
-    console.log("Current User updated:", currentUser);
+
     if (
       currentUser &&
       Array.isArray(currentUser.likes) &&
       currentUser.likes.length > 0
     ) {
       const ids = currentUser.likes.map((like) => like.advert.id);
-      console.log("Liked Advert IDs:", ids);
       setLikedAdvertIds(ids);
     } else {
-      console.log("No likes found or currentUser structure is not correct.");
       setLikedAdvertIds([]);
     }
   }, [currentUser]);
 
   useEffect(() => {
     if (likedAdvertIds.length > 0) {
-      console.log("Fetching liked adverts with IDs:", likedAdvertIds);
       dispatch(fetchLikedAdverts(likedAdvertIds));
     }
   }, [dispatch, likedAdvertIds]);
 
-  console.log("Liked Advert IDs state:", likedAdvertIds);
-
-  if (!Array.isArray(adverts)) {
-    console.error("Adverts is not an array:", adverts);
+  if (currentUser.likes.length > 0 && !Array.isArray(adverts)) {
     return null;
   }
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       {likedAdvertIds.length === 0 ? (
         <Typography
           sx={{
