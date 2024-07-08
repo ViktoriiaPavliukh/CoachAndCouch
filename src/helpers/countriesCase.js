271f5945a934ac81a3a7207a9bfd0a07d0c226ca
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-07-08 12:13:41 +0100

    fix countiesCase

--- src/helpers/countriesCase.js
+++ src/helpers/countriesCase.js
@@ -1,7 +1,22 @@
 export default function countriesCase(countrie) {
   if (!countrie) return "";
-  return countrie
-    .split(" ")
-    .map((el) => el[0].toUpperCase() + el.toLowerCase().substring(1))
-    .join(" ");
+  if (countrie.length === 3) {
+    return countrie.toUpperCase();
+  } else {
+    return countrie
+      .split(/(\s+|[-()])/)
+      .map((el) => {
+        if (
+          el.toLowerCase() === "і" ||
+          el.toLowerCase() === "та" ||
+          el.toLowerCase() === "в"
+        ) {
+          return el.toLowerCase();
+        } else if (el.toUpperCase() === "США") {
+          return el.toUpperCase();
+        }
+        return el.charAt(0).toUpperCase() + el.slice(1).toLowerCase();
+      })
+      .join("");
+  }
 }
