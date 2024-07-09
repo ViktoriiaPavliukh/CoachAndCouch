f8f1883941a68d20c07ec4c872c6cd85e70eb795
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-07-09 13:27:14 +0100

    fix fetching of data for countriesList  after reloading

--- src/components/PersonalAccount/Profile/ProfileData.jsx
+++ src/components/PersonalAccount/Profile/ProfileData.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "@/redux/users/operations";
+import { getCountries } from "@/redux/admin/operations"
 import {
   selectCurrentUser,
   selectUserIsLoading,
@@ -8,6 +9,7 @@ import {
 import { countriesSelector } from "@/redux/admin/adminSelector";
 import { Profile } from "../Profile";
 import Loader from "../../Loader/Loader";
+import { Typography } from "@mui/material";
 
 export function ProfileData() {
   const dispatch = useDispatch();
@@ -17,13 +19,51 @@ export function ProfileData() {
 
   useEffect(() => {
     dispatch(getCurrentUser());
-  }, [dispatch]);
+    if (!countriesList.length) {
+      dispatch(getCountries());
+    }
+  }, [dispatch, countriesList.length]);
+
+  useEffect(() => {
+    console.log("currentUser:", currentUser);
+    console.log("countriesList:", countriesList);
+  }, [currentUser, countriesList]);
 
   return isLoading ? (
     <Loader />
-  ) : currentUser ? (
-    <Profile currentUser={currentUser} countriesList={countriesList} />
-  ) : (
+  ) : !currentUser || countriesList.length === 0 ? (
     <Typography>No Data</Typography>
+  ) : (
+    <Profile currentUser={currentUser} countriesList={countriesList} />
   );
 }
+
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getCurrentUser } from "@/redux/users/operations";
+// import {
+//   selectCurrentUser,
+//   selectUserIsLoading,
+// } from "@/redux/users/selectors";
+// import { countriesSelector } from "@/redux/admin/adminSelector";
+// import { Profile } from "../Profile";
+// import Loader from "../../Loader/Loader";
+
+// export function ProfileData() {
+//   const dispatch = useDispatch();
+//   const currentUser = useSelector(selectCurrentUser);
+//   const isLoading = useSelector(selectUserIsLoading);
+//   const countriesList = useSelector(countriesSelector);
+
+//   useEffect(() => {
+//     dispatch(getCurrentUser());
+//   }, [dispatch]);
+
+//   return isLoading ? (
+//     <Loader />
+//   ) : currentUser ? (
+//     <Profile currentUser={currentUser} countriesList={countriesList} />
+//   ) : (
+//     <Typography>No Data</Typography>
+//   );
+// }
