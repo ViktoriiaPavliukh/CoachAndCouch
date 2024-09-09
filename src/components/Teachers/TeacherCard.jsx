b8f34dce249ebf18f242b8e9430d80bcc1758ed1
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-09-09 23:05:48 +0100

    change styles

--- src/components/Teachers/TeacherCard.jsx
+++ src/components/Teachers/TeacherCard.jsx
@@ -91,41 +91,65 @@ export function TeacherCard({ teacher }) {
               right: "20px",
             }}
           />
-          <CardContent sx={{ flexGrow: 1 }}>
+          <CardContent
+            sx={{
+              // flexGrow: 1,
+              display: "flex",
+              flexDirection: "column",
+              px: "16px",
+              py: 0,
+            }}
+          >
             <Stack
               direction="row"
               sx={{
                 justifyContent: "space-between",
                 alignItems: "center",
-                gap: "24px",
-                mb: "8px",
               }}
             >
-              <Stack
-                direction="row"
-                sx={{
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  gap: "10px",
-                }}
-              >
+              <Box direction="column">
                 <Typography
-                  // gutterBottom
-                  // variant="posterDescription"
-                  sx={{ fontWeight: "700", letterSpacing: "-0.003px" }}
+                  sx={{
+                    fontWeight: "400",
+                    fontSize: "18px",
+                    lineHeight: "156%",
+                  }}
                 >
-                  {teacher.user.firstName + " " + teacher.user.lastName}
+                  {teacher.user.firstName}
                 </Typography>
-              </Stack>
-
-              <Typography>ID:&nbsp;{teacher.id}</Typography>
+                <Typography
+                  sx={{
+                    fontWeight: "400",
+                    fontSize: "18px",
+                    lineHeight: "156%",
+                  }}
+                >
+                  {teacher.user.lastName}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  variant="posterStatus"
+                  sx={{
+                    display: "inline-block",
+                    color: (theme) => theme.palette.textColor.greyCard,
+                  }}
+                >
+                  <Box
+                    component="span"
+                    sx={{
+                      display: "inline-block",
+                      width: "6px",
+                      height: "6px",
+                      borderRadius: "50%",
+                      backgroundColor: "#0E5B1D",
+                      mr: "4px",
+                    }}
+                  />
+                  {intl.formatMessage({ id: "online" })}
+                </Typography>
+              </Box>
             </Stack>
-            <Typography
-              variant="posterItem"
-              sx={{ color: (theme) => theme.palette.textColor.grey }}
-            >
-              {intl.formatMessage({ id: "languagesTeaching" })}:
-            </Typography>
             {Boolean(teacher.teachingLanguages.length) && (
               <CategoryList
                 elements={
@@ -141,12 +165,12 @@ export function TeacherCard({ teacher }) {
                 display: "flex",
                 justifyContent: "flex-start",
                 flexDirection: "row",
-                marginBottom: "20px",
+                marginBottom: "12px",
               }}
             >
               <Typography
                 variant="posterItem"
-                sx={{ color: (theme) => theme.palette.textColor.grey }}
+                sx={{ color: (theme) => theme.palette.textColor.greyCard }}
               >
                 {intl.formatMessage({ id: "country" })}:&nbsp;
                 {en == "en"
@@ -162,24 +186,29 @@ export function TeacherCard({ teacher }) {
                     )}
               </Typography>
             </Stack>
-
             <Stack
               direction="row"
               sx={{
                 justifyContent: "space-between",
                 alignItems: "center",
-                height: "21px",
               }}
             >
-              <Box style={{ display: "flex", gap: "12px", pt: "4px" }}>
-                <Box sx={{ display: "flex", gap: "4px" }}>
+              <Box sx={{ display: "flex", gap: "12px" }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: "4px",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <StarBorderPurple500OutlinedIcon
                     sx={{
-                      fontSize: "16px",
-                      color: (theme) => theme.palette.textColor.darkGrey,
+                      fontSize: "18px",
+                      color: (theme) => theme.palette.textColor.fontColor,
                     }}
                   />
-                  <Typography variant="posterItem">
+                  <Typography variant="posterDescription">
                     {roundRating(teacher.user.rating)}
                   </Typography>
                 </Box>
@@ -204,7 +233,7 @@ export function TeacherCard({ teacher }) {
                       <FavoriteBorderOutlinedIcon
                         sx={{
                           fontSize: "16px",
-                          color: (theme) => theme.palette.textColor.darkGrey,
+                          color: (theme) => theme.palette.textColor.fontColor,
                         }}
                       />
                     )}
@@ -217,9 +246,9 @@ export function TeacherCard({ teacher }) {
                       />
                     )}
                     <Typography
-                      variant="posterItem"
+                      variant="posterDescription"
                       sx={{
-                        color: "rgba(0, 0, 0, 0.87)",
+                        color: (theme) => theme.palette.textColor.fontColor,
                       }}
                     >
                       {teacher.likes.length}
@@ -236,26 +265,6 @@ export function TeacherCard({ teacher }) {
                   <Typography variant="posterItem">156</Typography>
                 </Box> */}
               </Box>
-              <Box>
-                <Typography
-                  color="grey.700"
-                  variant="posterStatus"
-                  sx={{ display: "inline-block" }}
-                >
-                  <Box
-                    component="span"
-                    sx={{
-                      display: "inline-block",
-                      width: "6px",
-                      height: "6px",
-                      borderRadius: "50%",
-                      backgroundColor: "#0E5B1D",
-                      mr: "4px",
-                    }}
-                  />
-                  {intl.formatMessage({ id: "online" })}
-                </Typography>
-              </Box>
             </Stack>
           </CardContent>
         </CardActionArea>
@@ -267,7 +276,7 @@ export function TeacherCard({ teacher }) {
               width: "100vw",
               py: "12px",
               m: "8px",
-              mt: "0",
+              mt: "12px",
               borderRadius: "8px",
               color: (theme) => theme.palette.buttonColor.fontColor,
               fontSize: "14px",
