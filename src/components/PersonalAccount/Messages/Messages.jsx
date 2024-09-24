8c2e6873a35c21f1b3b217259403adcf907c7df9
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-09-24 11:14:36 +0300

    message fetch

--- src/components/PersonalAccount/Messages/Messages.jsx
+++ src/components/PersonalAccount/Messages/Messages.jsx
@@ -1,71 +1,47 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserMessages } from "@/redux/users/operations";
-import messages from "../../../defaults/conversations.json";
 import { selectMessages } from "@/redux/users/selectors";
 import { Box, Stack } from "@mui/material";
 
 import { ChatWithUser } from "./ChatWithUser";
-import { useEffect, useState } from "react";
 import { ChatList } from "./ChatList";
 import { selectUser } from "@/redux/auth/selectors";
 
-const sortedChats = messages.sort((a, b) => {
-  const lastMessageA = a.messages[a.messages.length - 1];
-  const lastMessageB = b.messages[b.messages.length - 1];
-  if (lastMessageA && lastMessageB) {
-    return new Date(lastMessageB.writtedAt) - new Date(lastMessageA.writtedAt);
-  }
-  if (lastMessageA) {
-    return -1;
-  } else if (lastMessageB) {
-    return 1;
-  }
-  return 0;
-});
-
 export const Messages = () => {
-  const fetchMessages = useSelector(selectMessages);
+  const fetchedMessages = useSelector(selectMessages);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  const [userChat, setUserChat] = useState(sortedChats[0]);
+  const [userChat, setUserChat] = useState(null);
+  const [allChats, setAllChats] = useState([]);
   const [isChatOpen, setIsChatOpen] = useState(false);
-  const userActive = userChat.userCorrespondenceId.id;
 
-  const uniqueIds = new Set();
-
-  fetchMessages.forEach((conversation) => {
-    conversation.messages.forEach((message) => {
-      if (message.senderId !== 12) {
-        uniqueIds.add(message.senderId);
-      }
-      if (message.receiverId !== 12) {
-        uniqueIds.add(message.receiverId);
+  const userActive =
+    userChat &&
+    userChat.messages.find((conversation) => {
+      if (conversation.senderId !== user.id) {
+        conversation.senderId;
+      } else {
+        conversation.receiverId;
       }
     });
-  });
 
-  const result = Array.from(uniqueIds);
-  console.log(result);
+  console.log(allChats);
+  // const uniqueIds = new Set();
 
-  console.log(fetchMessages);
+  // fetchedMessages.forEach((conversation) => {
+  //   conversation.messages.forEach((message) => {
+  //     if (message.senderId !== user.id) {
+  //       uniqueIds.add(message.senderId);
+  //     }
+  //     if (message.receiverId !== user.id) {
+  //       uniqueIds.add(message.receiverId);
+  //     }
+  //   });
+  // });
 
-  const sortedChats2 = fetchMessages.sort((a, b) => {
-    const lastMessageA = a.messages[a.messages.length - 1];
-    const lastMessageB = b.messages[b.messages.length - 1];
-    if (lastMessageA && lastMessageB) {
-      return (
-        new Date(lastMessageB.writtedAt) - new Date(lastMessageA.writtedAt)
-      );
-    }
-    if (lastMessageA) {
-      return -1;
-    } else if (lastMessageB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  console.log(sortedChats2);
+  // const result = Array.from(uniqueIds);
+  // console.log(result);
 
   const onGoBack = () => {
     setIsChatOpen(false);
@@ -76,11 +52,35 @@ export const Messages = () => {
 
   useEffect(() => {
     dispatch(getUserMessages());
-  }, []);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (fetchedMessages.length > 0) {
+      const sortedChats = [...fetchedMessages].sort((a, b) => {
+        const lastMessageA = a.messages[a.messages.length - 1];
+        const lastMessageB = b.messages[b.messages.length - 1];
+        if (lastMessageA && lastMessageB) {
+          return (
+            new Date(lastMessageB.writtedAt) - new Date(lastMessageA.writtedAt)
+          );
+        }
+        if (lastMessageA) {
+          return -1;
+        } else if (lastMessageB) {
+          return 1;
+        }
+        return 0;
+      });
+
+      console.log(sortedChats[0]);
+      setUserChat(sortedChats[0]);
+      setAllChats(sortedChats);
+    }
+  }, [fetchedMessages]);
 
   return (
     <Box sx={{ width: "100%" }}>
-      {sortedChats.length !== 0 ? (
+      {allChats.length !== 0 ? (
         <Stack
           sx={{
             display: { xs: "flex", md: "none" },
@@ -101,7 +101,7 @@ export const Messages = () => {
       ) : (
         <p> у вас ще немає повідомлень</p>
       )}
-      {sortedChats.length !== 0 ? (
+      {allChats.length !== 0 ? (
         <Stack
           sx={{
             display: { xs: "none", md: "flex" },
