60d681dabd3c84a073d3ad51eab29239709c8aa5
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-10-01 13:21:07 +0100

    add style to TeacherCard and CategoryList

--- src/components/Card/CategoryList.jsx
+++ src/components/Card/CategoryList.jsx
@@ -1,37 +1,46 @@
 import { PropTypes } from "prop-types";
-import { List, ListItem } from "@mui/material";
+import { List, ListItem, Box } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 
 export function CategoryList({ elements }) {
   return (
-    <List
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        my: "12px",
-        gap: "10px",
-        p: 0,
-      }}
-    >
-      {elements?.map((e) => (
-        <ListItem
-          key={uuidv4()}
-          sx={{
-            padding: "6px 14px",
-            backgroundColor: (theme) => theme.palette.background.languageBg,
-            borderRadius: "16px",
-            fontSize: "16px",
-            fontStyle: "normal",
-            fontWeight: "400",
-            lineHeight: "calc(16 / 12)",
-            width: "auto",
-            color: (theme) => theme.palette.textColor.listColor,
-          }}
-        >
-          {Object.values(e)}
-        </ListItem>
-      ))}
-    </List>
+    <Box>
+      <List
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          my: "12px",
+          gap: "10px",
+          p: 0,
+          flexWrap: "nowrap",
+          overflow: "auto",
+          whiteSpace: "nowrap",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+          scrollbarWidth: "none",
+        }}
+      >
+        {elements?.map((e) => (
+          <ListItem
+            key={uuidv4()}
+            sx={{
+              padding: "6px 14px",
+              backgroundColor: (theme) => theme.palette.background.languageBg,
+              borderRadius: "16px",
+              fontSize: "16px",
+              fontStyle: "normal",
+              fontWeight: "400",
+              lineHeight: "calc(16 / 12)",
+              width: "auto",
+              color: (theme) => theme.palette.textColor.listColor,
+            }}
+          >
+            {Object.values(e)}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 }
 CategoryList.propTypes = {
